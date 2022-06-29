95d9d7d0d6e0bb4348ce7e625ad7ac624ca2b6ea
中川　匠 <takumi@nakagawatakuminoMacBook-Air.local>
2022-06-29 16:34:38 +0900

    develop1 commit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,6 @@
 class RoomsController < ApplicationController
   def index
+    @rooms = Room.all
   end
 
   def show
@@ -10,10 +11,18 @@ class RoomsController < ApplicationController
   end
 
   def create
-    
+    room = Room.new(room_params)
+    room.save!
+    redirect_to home_top_url, notice:"宿を登録しました。"
   end
   
 
   def edit
   end
+
+  private
+  def room_params
+    params.require(:room).permit(:name, :introduction, :price, :location, :room_image)   
+  end
+  
 end
